ade277d936dc8efeab6354b11445b576331a365a
BLBC <53979731+hjk789@users.noreply.github.com>
2020-12-24 22:42:12 -0300

    J/U/YMRRH: Bug fixes; improved performance; max repetitions now 2
    - Improved performance;
    - Changed the max number of repetitions to 2;
    - Fixed timing bug that made the script increment and hide the wrong videos;
    - Fixed bug that was requiring a video to be recommended 4 times before being hidden;
    - Fixed bug that was leaving an empty space where a video was hidden;

--- YouTube-Mobile-Repeated-Recommendations-Hider/YouTube-Mobile-Repeated-Recommendations-Hider.user.js
+++ YouTube-Mobile-Repeated-Recommendations-Hider/YouTube-Mobile-Repeated-Recommendations-Hider.user.js
@@ -1,43 +1,46 @@
 // ==UserScript==
-// @name			YouTube Mobile Repeated Recommendations Hider
-// @description		Hides any videos that are recommended more than 3 times at the mobile homepage
-// @version			1.0
-// @author			BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
-// @copyright		2020+, BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
-// @homepage		https://github.com/hjk789/Creations/tree/master/Userscripts/YouTube-Mobile-Repeated-Recommendations-Hider
-// @license			https://github.com/hjk789/Creations/tree/master/Userscripts/YouTube-Mobile-Repeated-Recommendations-Hider#license
-// @match			https://m.youtube.com
-// @grant			GM.setValue
-// @grant			GM.getValue
+// @name            YouTube Mobile Repeated Recommendations Hider
+// @description     Hides any videos that are recommended more than 2 times at the mobile homepage
+// @version         1.2
+// @author          BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
+// @copyright       2020+, BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
+// @homepage        https://github.com/hjk789/Creations/tree/master/Userscripts/YouTube-Mobile-Repeated-Recommendations-Hider
+// @license         https://github.com/hjk789/Creations/tree/master/Userscripts/YouTube-Mobile-Repeated-Recommendations-Hider#license
+// @match           https://m.youtube.com
+// @grant           GM.setValue
+// @grant           GM.getValue
 // ==/UserScript==
 
-setInterval(function()
+main()
+
+async function main()
 {
-	let titlesList = document.querySelectorAll(".large-media-item-metadata h3")
-	
-	for (let i=0; i < titlesList.length; i++)
-	{
-		let videoTitle = titlesList[i]
-		let videoTitleText = videoTitle.textContent
+    let titlesList = document.getElementsByClassName("large-media-item-metadata")
 
-		GM.getValue(videoTitleText).then(function(value)
-		{
-			if (typeof value == "undefined")
-				value = 1
-			else
-			{
-				if (value > 3)
-				{
-					let videoItem = videoTitle.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
-					videoItem.style.display = "none"					
-				}
+    while (titlesList.length > 0)
+    {
+        let videoTitle = titlesList[0].firstChild.firstChild
+        let videoTitleText = videoTitle.textContent
 
-				value++
-			}
+        let value = await GM.getValue(videoTitleText)
 
-            videoTitle.parentElement.parentElement.className = ""
+        if (typeof value == "undefined")
+            value = 1
+        else
+        {
+            if (value >= 2)
+            {
+                let videoItem = videoTitle.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+                videoItem.style.display = "none"
+            }
 
-			GM.setValue(videoTitleText, value)
-		})
-	}
-}, 500)
+            value++
+        }
+
+        titlesList[0].className = ""
+
+        GM.setValue(videoTitleText, value)
+    }
+
+    setTimeout(() => main(), 500)
+}
