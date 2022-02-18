dd249b60bfb812d1c5f1c081b4eec22a82e9ef48
BLBC <53979731+hjk789@users.noreply.github.com>
2022-02-18 05:35:14 +0000

    YCBB: New "Peek audio transcription"; Fixed menu not appearing playlist
    - Added new "Peek audio transcription". While the "Peek video content" feature enables you to check the *visual* content of the video, this new feature enables you to check the *verbal* content. This is most useful for videos that are just someone talking about something, or videos that you don't know exactly what it is about and you just need to peek the first spoken sentences where the presenter explains it. You can also click the timestamp to open the video directly in the part you are interested in.
    - Fixed the menu not appearing on playlists.

--- YouTube-Clickbait-Buster/YouTube-Clickbait-Buster.user.js
+++ YouTube-Clickbait-Buster/YouTube-Clickbait-Buster.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            YouTube Clickbait-Buster
-// @version         1.1.0
+// @version         1.2.0
 // @description     Check whether it's worth watching a video by peeking it's content, viewing the thumbnail in full-size and displaying the full title. Works on both YouTube's desktop and mobile layouts.
 // @author          BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
 // @copyright       2022+, BLBC (github.com/hjk789, greasyfork.org/users/679182-hjk789)
@@ -14,24 +14,30 @@
 
 //*********** SETTINGS ***********
 
-const numColumns = 1        // The video storyboard YouTube provides is divided in chunks of frames. Set this to the number of chunks per row you would like. Note that the size
-                            // and the number of chunks per row is limited by your device's screen dimensions. You can open the chunk in a new tab to view it in full-size.
+const numColumns = 1                     // The video storyboard YouTube provides is divided in chunks of frames. Set this to the number of chunks per row you would like. Note that the size
+                                         // and the number of chunks per row is limited by your device's screen dimensions. You can open the chunk in a new tab to view it in full-size.
 
-const fullTitles = true     // Whether the videos' title should be forced to be displayed in full, without any trimmings. In case you are using any other userscript
-                            // or extension that changes YouTube's layout, set this to false if you see anything wrong in the layout, such as titles overlapping.
+const fullTitles = true                  // Whether the videos' title should be forced to be displayed in full, without any trimmings. In case you are using any other userscript
+                                         // or extension that changes YouTube's layout, set this to false if you see anything wrong in the layout, such as titles overlapping.
+
+const preferredTranscriptLanguage = ""   // The two letters language-country code of the language you want the transcriptions to always be in. Examples: for US English, en-US,
+                                         // for Spain's Spanish, es-ES, and so on. If there's no subtitles in the specified language, another language (if any) will be selected
+                                         // and then translated to the specified language. If left blank, the language most likely to be the original is selected.
 
 //********************************
 
 
 let selectedVideoURL
-let viewStoryboardButton, viewThumbnailButton
+let viewStoryboardButton, viewThumbnailButton, viewTranscriptButton
 const isMobile = !/www/.test(location.hostname)
 let isMenuReady = false
 
 /* Add the styles */
 {
     const style = document.createElement("style")
-    style.innerHTML = "#viewStoryboardButton:hover, #viewThumbnailButton:hover { background-color: var(--yt-spec-10-percent-layer) !important; }"             // Add the desktop version's hover for the recommendation menu items
+    style.innerHTML = "#viewStoryboardButton:hover, #viewThumbnailButton:hover, #viewTranscriptButton:hover { background-color: var(--yt-spec-10-percent-layer) !important; }" +            // Add the desktop version's hover for the recommendation menu items
+                      "#transcriptTextContainer transcript { width: max-content; max-width: 96vw; display: block; left: 0; right: 0; position: relative; margin: auto; }" +
+                      "transcript text { display: block; margin-top: 10px; }"
 
     if (fullTitles)
         style.innerHTML += "#video-title, h3, h4 { max-height: initial !important; -webkit-line-clamp: initial !important; }"                // The full video title style
@@ -52,6 +58,11 @@ let isMenuReady = false
         const thumbnail = document.getElementById("highresThumbnail")
 
         if (thumbnail)  thumbnail.remove()
+
+
+        const transcript = document.getElementById("transcriptTextContainer")
+
+        if (transcript)  transcript.parentElement.remove()
     })
 }
 
@@ -107,6 +118,135 @@ let isMenuReady = false
         xhr.send()
 
         document.body.click()     // Dismiss the menu.
+    }
+
+    viewTranscriptButton = document.createElement(isMobile ? "button" : "div")
+    viewTranscriptButton.id = "viewTranscriptButton"
+    viewTranscriptButton.className = viewStoryboardButton.className
+    viewTranscriptButton.style = viewStoryboardButton.style.cssText
+    viewTranscriptButton.innerHTML = "Peek audio transcription"
+    viewTranscriptButton.onclick = function()
+    {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', selectedVideoURL)
+        xhr.onload = function()
+        {
+            let transcriptObj = xhr.responseText.match(/"playerCaptionsTracklistRenderer":({"captionTracks":\[{"baseUrl".+?}\].+?}.+?\].+?})},"videoDetails"/)
+
+            if (!transcriptObj)
+            {
+                alert("Transcript not available for this video!")
+
+                return
+            }
+
+            transcriptObj = JSON.parse(unescape(decodeURI(transcriptObj[1])).replaceAll("\\u0026","&"))
+
+            const backgroundColor = "var(--yt-spec-brand-background-solid, "+ getComputedStyle(document.documentElement).backgroundColor +")"
+
+            const transcriptContainer = document.createElement("div")
+            transcriptContainer.style = "position: fixed; z-index: 9999; background-color: " + backgroundColor + "; color: var(--paper-listbox-color); font-size: 15px;" +
+                                        "max-height: 98vh; width: max-content; max-width: 94vw; overflow: scroll; top: 0; left: 0; right: 0; margin: auto; padding: 10px;"
+            transcriptContainer.onclick = function() { event.stopPropagation() }
+
+            const transcriptLanguageLabel = document.createElement("div")
+            transcriptLanguageLabel.innerText = "Transcript language: "
+            transcriptLanguageLabel.style = "margin-bottom: 5px; width: max-content;"
+            transcriptContainer.appendChild(transcriptLanguageLabel)
+
+            const transcriptLanguageDropdown = document.createElement("select")
+            transcriptLanguageDropdown.style = "background-color: " + backgroundColor + "; color: var(--paper-listbox-color); border: 1px solid lightgray; border-radius: 5px; padding: 3px;"
+            transcriptLanguageDropdown.onchange = function() { loadTranscript(transcriptObj.captionTracks[this.selectedIndex].baseUrl) }
+            transcriptLanguageLabel.appendChild(transcriptLanguageDropdown)
+
+            for (let i=0; i < transcriptObj.captionTracks.length; i++)
+            {
+                const option = document.createElement("option")
+                option.innerText = isMobile ? transcriptObj.captionTracks[i].name.runs[0].text : transcriptObj.captionTracks[i].name.simpleText
+                option.value = transcriptObj.captionTracks[i].languageCode
+
+                transcriptLanguageDropdown.appendChild(option)
+            }
+
+            transcriptLanguageDropdown.value = preferredTranscriptLanguage
+
+            if (preferredTranscriptLanguage && !transcriptLanguageDropdown.value)
+                transcriptLanguageDropdown.value = preferredTranscriptLanguage.split("-")[0]
+
+            if (!transcriptLanguageDropdown.value)
+            {
+                if (transcriptObj.captionTracks.length > 1)
+                {
+                    const index = transcriptObj.audioTracks[0].captionTrackIndices[transcriptObj.audioTracks[0].captionTrackIndices.length-1]
+                    const autogen = transcriptObj.captionTracks[index]
+
+                    if (autogen.vssId[0] == "a")
+                    {
+                        const a = autogen.vssId.split(".")[1]
+
+                        if (transcriptObj.captionTracks[index+1] && transcriptObj.captionTracks[index+1].vssId.includes(a))
+                            transcriptLanguageDropdown.value = transcriptObj.captionTracks[index+1].languageCode
+                        else if (transcriptObj.captionTracks[index-1])
+                            transcriptLanguageDropdown.value = transcriptObj.captionTracks[index-1].languageCode
+                        else
+                            transcriptLanguageDropdown.value = transcriptObj.captionTracks[transcriptObj.audioTracks[0].captionTrackIndices[0]].languageCode
+                    }
+                }
+                else transcriptLanguageDropdown.value = transcriptLanguageDropdown.options[0].value
+            }
+
+
+            const transcriptTranslationLabel = document.createElement("div")
+            transcriptTranslationLabel.innerText = "Translate to: "
+            transcriptTranslationLabel.style = "margin-top: 10px; padding-bottom: 10px;"
+            transcriptContainer.appendChild(transcriptTranslationLabel)
+
+            const transcriptTranslationDropdown = document.createElement("select")
+            transcriptTranslationDropdown.style = "margin-left: 53px; background-color: " + backgroundColor + "; color: var(--paper-listbox-color); border: 1px solid lightgray; border-radius: 5px; padding: 3px;"
+            transcriptTranslationDropdown.onchange = function() { loadTranscript(transcriptObj.captionTracks[transcriptLanguageDropdown.selectedIndex].baseUrl + "&tlang=" + this.value) }
+            transcriptTranslationLabel.appendChild(transcriptTranslationDropdown)
+
+            const emptyOption = document.createElement("option")
+            transcriptTranslationDropdown.appendChild(emptyOption)
+
+            for (let i=0; i < transcriptObj.translationLanguages.length; i++)
+            {
+                const option = document.createElement("option")
+                option.innerText = isMobile ? transcriptObj.translationLanguages[i].languageName.runs[0].text : transcriptObj.translationLanguages[i].languageName.simpleText
+                option.value = transcriptObj.translationLanguages[i].languageCode
+
+                transcriptTranslationDropdown.appendChild(option)
+            }
+
+            transcriptTranslationDropdown.value = preferredTranscriptLanguage
+
+            if (!transcriptTranslationDropdown.value)
+                transcriptTranslationDropdown.value = preferredTranscriptLanguage.split("-")[0]
+
+
+            const transcriptTextContainer = document.createElement("div")
+            transcriptTextContainer.id = "transcriptTextContainer"
+            transcriptTextContainer.style = "min-width: max-content; max-width: 94vw; border-top: 1px solid lightgray;"
+            transcriptContainer.appendChild(transcriptTextContainer)
+
+            if (isMobile)
+            {
+                const closeButton = document.createElement("div")
+                closeButton.innerText = "X"
+                closeButton.style = "position: sticky; width: 25px; float: right; bottom: 0px; background-color: #f0f0f0ab; border-radius: 7px; padding: 10px 15px; text-align: center; font-size: 25px;"
+                closeButton.onclick = function() { document.body.click() }
+                transcriptContainer.appendChild(closeButton)
+            }
+
+
+            document.body.appendChild(transcriptContainer)
+
+
+            loadTranscript(transcriptObj.captionTracks[transcriptLanguageDropdown.selectedIndex].baseUrl + "&tlang=" + transcriptTranslationDropdown.value)
+        }
+        xhr.send()
+
+        document.body.click()
     }
 
     viewThumbnailButton = document.createElement(isMobile ? "button" : "div")
@@ -167,6 +307,7 @@ let isMenuReady = false
 
     }
 
+
     viewStoryboardButton.style.borderTop = "solid 1px var(--yt-spec-10-percent-layer, #ddd)"             // Add a separator between Youtube's menu items and the ones added by the script.
 
 }
@@ -177,8 +318,8 @@ main()
 
 function main()
 {
-    const videosSelector = isMobile ? "ytm-rich-item-renderer, ytm-video-with-context-renderer, ytm-compact-video-renderer, ytm-compact-playlist-renderer, ytm-compact-show-renderer"
-                                    : "ytd-rich-item-renderer, ytd-grid-video-renderer, ytd-compact-video-renderer, ytd-compact-playlist-renderer, ytd-compact-movie-renderer, ytd-video-renderer, ytd-compact-radio-renderer"
+    const videosSelector = isMobile ? "ytm-rich-item-renderer, ytm-video-with-context-renderer, ytm-compact-video-renderer, ytm-compact-playlist-renderer, ytm-compact-show-renderer, ytm-playlist-video-renderer"
+                                    : "ytd-rich-item-renderer, ytd-grid-video-renderer, ytd-compact-video-renderer, ytd-compact-playlist-renderer, ytd-compact-movie-renderer, ytd-playlist-video-renderer, ytd-video-renderer, ytd-compact-radio-renderer"
 
     addRecommendationMenuItems()
 
@@ -195,7 +336,7 @@ function addRecommendationMenuItems()
 {
     const waitForRecommendationMenu = setInterval(function()
     {
-        const recommendationMenu = isMobile ? document.getElementById("menu") : document.querySelector("#details #menu yt-icon, .details #menu yt-icon, #title-wrapper #menu yt-icon")
+        const recommendationMenu = isMobile ? document.getElementById("menu") : document.querySelector("#details #menu yt-icon, .details #menu yt-icon, #title-wrapper #menu yt-icon, ytd-playlist-video-renderer #menu yt-icon")
 
         if (!recommendationMenu)
             return
@@ -209,6 +350,7 @@ function addRecommendationMenuItems()
         if (isMobile)
         {
             recommendationMenu.firstChild.appendChild(viewStoryboardButton)
+            recommendationMenu.firstChild.appendChild(viewTranscriptButton)
             recommendationMenu.firstChild.appendChild(viewThumbnailButton)
         }
         else
@@ -234,6 +376,7 @@ function addRecommendationMenuItems()
 
 
                 recommendationMenuItem.parentElement.appendChild(viewStoryboardButton)
+                recommendationMenuItem.parentElement.appendChild(viewTranscriptButton)
                 recommendationMenuItem.parentElement.appendChild(viewThumbnailButton)
 
                 if (!isMenuReady)
@@ -244,12 +387,12 @@ function addRecommendationMenuItems()
                     isMenuReady = true
                 }
 
-                const isChannelPage = location.pathname.includes("/channel/") || location.pathname.includes("/user/") || location.pathname.includes("/c/")
+                const isChannelOrPlaylistPage = location.pathname.includes("/channel/") || location.pathname.includes("/user/") || location.pathname.includes("/c/") || location.pathname == "/playlist"
 
-                if (isChannelPage && !document.querySelector("ytd-guide-signin-promo-renderer"))             // In the channel page, when the user is signed in, Youtube already adds a separator at the bottom of the menu.
-                    viewStoryboardButton.style.borderTop = ""                                                // This removes the separator when on these pages.
+                if (isChannelOrPlaylistPage && document.querySelector("ytd-topbar-menu-button-renderer #avatar-btn"))             // In the channel page, when the user is signed in, Youtube already adds a separator at the bottom of the menu.
+                    viewStoryboardButton.style.borderTop = ""                                                                     // This removes the separator when on these pages.
                 else
-                    viewStoryboardButton.style.borderTop = "solid 1px var(--yt-spec-10-percent-layer, #ddd)"                                  // And adds it back when the user switches to another non-channel page.
+                    viewStoryboardButton.style.borderTop = "solid 1px var(--yt-spec-10-percent-layer, #ddd)"                      // And adds it back when the user switches to another non-channel page.
 
             }, 100)
         }
@@ -330,6 +473,37 @@ function createStoryboardImg(num, container, urlSplit, param, mode)
 
     container.appendChild(img)
 
+}
+
+function loadTranscript(url)
+{
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.onload = function()
+    {
+        const transcriptTextContainer = document.getElementById("transcriptTextContainer")
+
+        transcriptTextContainer.innerHTML = xhr.responseText.replaceAll("&amp;#39;", "'").replaceAll("&amp;quot;", '"')
+
+        const lines = transcriptTextContainer.firstElementChild.children
+
+        for (let i=0; i < lines.length; i++)
+        {
+            const seconds = lines[i].attributes.start.value.split(".")[0]
+
+            const date = new Date(null)
+            date.setSeconds(seconds)
+            const time = seconds >= 3600 ? date.toISOString().substr(11, 8) : date.toISOString().substr(14, 5)
+
+            const timestamp = document.createElement("a")
+            timestamp.href = selectedVideoURL + "&t=" + seconds
+            timestamp.style = "background-color: var(--yt-spec-10-percent-layer); color: var(--paper-listbox-color); padding: 2px 5px 1px 5px; margin-right: 10px; text-decoration: none;"
+            timestamp.innerText = time
+
+            lines[i].insertBefore(timestamp, lines[i].firstChild)
+        }
+    }
+    xhr.send()
 }
 
 function cleanVideoUrl(fullUrl)
