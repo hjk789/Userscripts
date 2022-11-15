82b91c3584c3adfa8c513b6399a1b2951c34edd1
BLBC <BLBCDev@outlook.com>
2022-11-15 04:55:46 -0300

    RDTP: New user n sub pages; New duplicates filter; New time selector
    - Added ability also specify users to fetch posts from;
    - Now the script also works on specific subreddits and user pages. Just open the subreddit or user page and the script will automatically load the contents of just this specific sub or user;
    - Added a duplicates filter which excludes any reposted image or video since when you opened the page. Note that this only works for *exact* reposts, i.e the same file reuploaded, it doesn't calculate the level of similarity, it only checks if it's the same file.
    - Added a time window selector at the top of the viewport, so that you can change it dynamically without having to edit and reload the script.

--- Reddit-Distributed-Top-Posts/Reddit-Distributed-Top-Posts.user.js
+++ Reddit-Distributed-Top-Posts/Reddit-Distributed-Top-Posts.user.js
@@ -7,16 +7,19 @@
 // @homepage        https://github.com/hjk789/Userscripts/tree/master/Reddit-Distributed-Top-Posts
 // @license         https://github.com/hjk789/Userscripts/tree/master/Reddit-Distributed-Top-Posts#license
 // @match           https://*.reddit.com/
+// @include         /https://\w+\.reddit\.com/(r|user)/\w+/?$/
 // @grant           none
 // ==/UserScript==
 
 
 //*********** SETTINGS ***********
 
-const subs = ["memes", "gifs", "aww"]       // The list of subreddits to gather the posts from. You must use the sub's id (the one after the r/).
+let subs = ["memes", "gifs", "aww"]         // The list of subreddits to gather the posts from. You must use the sub's id (the one after the r/).
                                             // You can specify as many communities as you want, you just need to follow the syntax ["Sub1", "Sub2"]
 
-const timeWindow = "day"                    // The time window of the top posts. Accepted values are "hour", "day", "week", "month", "year" and "alltime".
+let users = []                              // Same as above, but for user pages (the ones at reddit.com/user/<username>).
+
+let timeWindow = "day"                      // The time frame to get the top posts from. Accepted values are "hour", "day", "week", "month", "year" and "alltime".
 
 const maxVideoQuality = 720                 // Reddit provides multiple resolutions for the same video. The script will load the videos in
                                             // the quality you specified (if available). Accepted qualities are 240, 360, 480, 720 and 1080.
@@ -30,8 +33,9 @@ const includeNSFW = false                   // When false, posts set as NSFW are
 
 
 
-const responsesPerSub = []
+const responsesPerSource = {}
 let loading = false
+const duplicateMediaSamples = []
 
 const container = document.createElement("div")
 container.style = "position: fixed; z-index: 999; inset: 0px; margin: auto; height: 100vh; width: min(900px,100vw); overflow-y: scroll; background: white; text-align: center;"
@@ -44,6 +48,42 @@ container.onscroll = async function()
         loadNextPage()
     }
 }
+
+const timeFrames = ["hour", "day", "week", "month", "year", "alltime"]
+
+const timeWindowContainer = document.createElement("div")
+timeWindowContainer.style = "position: sticky; top: 0px; background: white; padding: 4px;"
+
+const label = document.createElement("span")
+label.innerText = "Top posts since a "
+timeWindowContainer.appendChild(label)
+
+const timeWindowDropdown = document.createElement("select")
+timeWindowDropdown.onchange = function() 
+{
+    timeWindow = this.value
+    duplicateMediaSamples.length = 0
+    
+    container.innerHTML = ""
+    container.appendChild(timeWindowContainer)
+    timeWindowDropdown.value = timeWindow
+    
+    loadPosts()    
+}
+
+for (let i=0; i < timeFrames.length; i++)
+{
+    const option = document.createElement("option")
+    option.value = timeFrames[i]
+    option.innerText = timeFrames[i].charAt(0).toUpperCase() + timeFrames[i].slice(1)
+    timeWindowDropdown.appendChild(option)
+}
+
+timeWindowDropdown.value = timeWindow
+
+timeWindowContainer.appendChild(timeWindowDropdown)
+container.appendChild(timeWindowContainer)
+      
 document.body.appendChild(container)
 
 
@@ -57,8 +97,27 @@ const onViewObserver = new IntersectionObserver((entries) =>            // when 
             entry.target.pause()
     })
 
-}, {threshold: 0.9})
+}, {threshold: 0.8})
 
+
+if (/\/(r|user)\//.test(location.pathname))
+{
+    const subOrUserName = location.pathname.split("/")[2]
+
+    if (location.pathname.includes("/user/"))
+    {
+        subs = []
+        users = [subOrUserName]
+    }
+    else
+    {
+        subs = [subOrUserName]
+        users = []
+    }
+}
+
+
+const sourceNames = subs.concat(users)
 
 loadPosts()
 
@@ -67,20 +126,23 @@ loadPosts()
 async function loadPosts()
 {
     for (let i=0; i < subs.length; i++)
-        responsesPerSub.push(await fetchSubredditPostsPromise(subs[i]))
+        responsesPerSource[subs[i]] = { type: "sub", response: await fetchSubredditPostsPromise("subreddits/"+subs[i]) }
+    
+    for (let i=0; i < users.length; i++)
+        responsesPerSource[users[i]] = { type: "user", response: await fetchSubredditPostsPromise("user/"+users[i]+"/posts") }
 
     processPosts()
 }
 
-function fetchSubredditPostsPromise(subName, after)
+function fetchSubredditPostsPromise(sourceUrlString, after)
 {
-    return new Promise(resolve => fetchSubredditPosts(subName, after, resolve))
+    return new Promise(resolve => fetchSubredditPosts(sourceUrlString, after, resolve))
 }
 
-function fetchSubredditPosts(subName, after, resolve)
+function fetchSubredditPosts(sourceUrlString, after, resolve)
 {
     const xhr = new XMLHttpRequest()
-    xhr.open("GET", "https://gateway.reddit.com/desktopapi/v1/subreddits/"+subName+"?limit=3&sort=top&t="+timeWindow+"&allow_over18="+(+includeNSFW)+"&after="+after)
+    xhr.open("GET", "https://gateway.reddit.com/desktopapi/v1/"+sourceUrlString+"?limit=3&sort=top&t="+timeWindow+"&allow_over18="+(+includeNSFW)+"&after="+after)
     xhr.onload = function()
     {
         const response = JSON.parse(xhr.responseText)
@@ -89,7 +151,7 @@ function fetchSubredditPosts(subName, after, resolve)
 
         resolve(response)
     }
-    xhr.onerror = ()=> setTimeout(()=> fetchSubredditPosts(subName, after, resolve), 5000)
+    xhr.onerror = ()=> setTimeout(()=> fetchSubredditPosts(sourceUrlString, after, resolve), 5000)
 
     xhr.send()
 }
@@ -100,13 +162,13 @@ function processPosts()
 
     for (let i=0; i < 3; i++)
     {
-        for (let j=0; j < responsesPerSub.length; j++)
+        for (let j=0; j < sourceNames.length; j++)
         {
-            const response = responsesPerSub[j]
+            const response = responsesPerSource[sourceNames[j]].response
 
             const post = response.posts[response.postIds[i]]
 
-            if (!post || post.media && post.media.type == "text" || !post.media && !post.source)
+            if (!post || post.media && (!post.media.type || post.media.type == "text" || post.media.provider == "YouTube") || !post.media && !post.source)
                 continue
 
             if (!post.media)
@@ -119,26 +181,40 @@ function processPosts()
 
                 post.source.url = post.source.url.replace("https://imgur", "https://i.imgur")
 
-                if (imageExtensions.includes(sourceExtension))
+                const xhr = new XMLHttpRequest()
+                xhr.open('GET', post.source.url)
+                xhr.onload = function()
                 {
-                    const img = document.createElement("img")
-                    img.src = post.source.url
-                    img.style = style
-                    img.onload = function() { checkAndResize(this) }
-                    container.appendChild(img)
-                }
-                else if (videoExtensions.includes(sourceExtension))
-                {
-                    const video = document.createElement("video")
-                    video.src = post.source.url.replace("gifv", "mp4")
-                    video.style = style
-                    video.controls = true
-                    video.onloadeddata = function() { checkAndResize(this, true) }
-                    container.appendChild(video)
+                    const hash = stringToHash(this.response)
+                    
+                    if (duplicateMediaSamples.includes(hash))
+                        return
+                    
+                    duplicateMediaSamples.push(hash)
+                    
+                    if (imageExtensions.includes(sourceExtension))
+                    {
+                        const img = document.createElement("img")
+                        img.src = post.source.url
+                        img.style = style
+                        img.onload = function() { checkAndResize(this) }
+                        container.appendChild(img)
+                    }
+                    else if (videoExtensions.includes(sourceExtension))
+                    {
+                        const video = document.createElement("video")
+                        video.src = post.source.url.replace("gifv", "mp4")
+                        video.style = style
+                        video.controls = true
+                        video.onloadeddata = function() { checkAndResize(this, true) }
+                        container.appendChild(video)
 
-                    onViewObserver.observe(video)
+                        onViewObserver.observe(video)
+                    }
+                    
+                    createPostMetadata(post, container)
                 }
-                else continue
+                xhr.send()
             }
             else if (post.media.type.includes("video") || post.media.type == "embed" || post.media.type == "image" && post.media.videoPreview)
             {
@@ -150,12 +226,12 @@ function processPosts()
                     videoUrl = post.media.content
                 else
                     videoUrl = videoRoot.scrubberThumbSource.replace("_96.", (videoRoot.height > maxVideoQuality ? "_"+ maxVideoQuality +"." : "_"+videoRoot.height+"."))
-
+                
+                let audio
                 const video = document.createElement("video")
                 video.src = videoUrl
                 video.style = style
                 video.controls = true
-                video.onloadeddata = function() { checkAndResize(this, true) }
                 video.onerror = function()
                 {
                     video.onerror = function()
@@ -172,24 +248,47 @@ function processPosts()
 
                     this.src = this.src.replace(/_\d+\./, "_480.")
                 }
+                video.onloadeddata = function() 
+                { 
+                    checkAndResize(this, true)
+                    
+                    if (isVideo)
+                    {
+                        audio = document.createElement("audio")
+                        audio.src = post.media.scrubberThumbSource.replace("_96.", "_audio.")
+                        container.appendChild(audio)
 
-                if (isVideo)
-                {
-                    const audio = document.createElement("audio")
-                    audio.src = post.media.scrubberThumbSource.replace("_96.", "_audio.")
-                    container.appendChild(audio)
+                        video.onplay = ()=> { audio.play(); audio.currentTime = video.currentTime }
+                        video.onpause = ()=> audio.pause()
+                    }
 
-                    video.onplay = ()=> { audio.play(); audio.currentTime = video.currentTime }
-                    video.onpause = ()=> audio.pause()
+                    const xhr = new XMLHttpRequest()
+                    xhr.open('GET', this.src)
+                    xhr.onload = function()
+                    {
+                        const hash = stringToHash(this.response)
+
+                        if (duplicateMediaSamples.includes(hash))
+                        {
+                            video.remove()
+                            audio.remove()
+                            return
+                        }
+
+                        duplicateMediaSamples.push(hash)
+                        
+                        container.appendChild(video)
+
+                        createPostMetadata(post, container)
+                        
+                        onViewObserver.observe(video)
+                    }
+                    xhr.send()
                 }
-
-                container.appendChild(video)
-
-                onViewObserver.observe(video)
             }
             else if (post.media.type == "image")
             {
-                let mediaUrl = post.media.content
+                let mediaUrl = post.media.content.startsWith("http") ? post.media.content : post.preview.url
 
                 if (post.media.height > maxImageHeight)
                 {
@@ -203,11 +302,26 @@ function processPosts()
                     }
                 }
 
-                const img = document.createElement("img")
-                img.src = mediaUrl
-                img.style = style
-                img.onload = function() { checkAndResize(this) }
-                container.appendChild(img)
+                const xhr = new XMLHttpRequest()
+                xhr.open('GET', mediaUrl)
+                xhr.onload = function()
+                {
+                    const hash = stringToHash(this.response)
+                    
+                    if (duplicateMediaSamples.includes(hash))
+                        return
+                    
+                    duplicateMediaSamples.push(hash)
+                    
+                    const img = document.createElement("img")
+                    img.src = mediaUrl
+                    img.style = style
+                    img.onload = function() { checkAndResize(this) }
+                    container.appendChild(img)
+                    
+                    createPostMetadata(post, container)
+                }
+                xhr.send()
             }
             else if (post.media.type == "gallery")
             {
@@ -227,32 +341,28 @@ function processPosts()
                         }
                     }
 
-                    const img = document.createElement("img")
-                    img.src = mediaUrl
-                    img.style = style
-                    img.onload = function() { checkAndResize(this) }
-                    container.appendChild(img)
+                    const xhr = new XMLHttpRequest()
+                    xhr.open('GET', mediaUrl, false)
+                    xhr.onload = function()
+                    {
+                        const hash = stringToHash(this.response)
+
+                        if (duplicateMediaSamples.includes(hash))
+                            return
+
+                        duplicateMediaSamples.push(hash)
+
+                        const img = document.createElement("img")
+                        img.src = mediaUrl
+                        img.style = style
+                        img.onload = function() { checkAndResize(this) }
+                        container.appendChild(img)
+                    }
+                    xhr.send()
                 })
+                
+                createPostMetadata(post, container)
             }
-
-
-            const subname = document.createElement("a")
-            subname.style = "margin-top: -10px; margin-right: 50px; display: ruby-text;"
-            subname.innerText = "r/"+post.permalink.split("/r/")[1].split("/")[0]
-            subname.href = "/./"+subname.innerText
-            container.appendChild(subname)  
-          
-            const username = document.createElement("a")
-            username.style = "margin-top: -10px; margin-right: 50px; display: ruby-text;"
-            username.innerText = "u/"+post.author
-            username.href = username.innerText
-            container.appendChild(username)
-
-            const commentCount = document.createElement("a")
-            commentCount.style = "margin-top: -10px; display: ruby-text;"
-            commentCount.innerText = post.numComments +" comments"
-            commentCount.href = post.permalink
-            container.appendChild(commentCount)
         }
     }
 }
@@ -275,20 +385,57 @@ function checkAndResize(element, isVideo)
 
 async function loadNextPage()
 {
-    for (let i=0; i < subs.length; i++)
+    for (let i=0; i < sourceNames.length; i++)
     {
-        if (!responsesPerSub[i].token)
+        const response = responsesPerSource[sourceNames[i]]
+
+        if (!response.response.token)
         {
-            responsesPerSub.splice(i, 1)
-            subs.splice(i, 1)
+            delete responsesPerSource[sourceNames[i]]
+            sourceNames.splice(i, 1)
             i--
             continue
         }
 
-        responsesPerSub[i] = await fetchSubredditPostsPromise(subs[i], responsesPerSub[i].token)
+        response.response = await fetchSubredditPostsPromise(response.type == "sub" ? "subreddits/"+sourceNames[i] : "user/"+sourceNames[i]+"/posts", response.response.token)
     }
 
     processPosts()
 
     loading = false
+}
+
+function createPostMetadata(post, container)
+{
+    const subname = document.createElement("a")
+    subname.style = "margin-top: -10px; margin-right: 50px; display: ruby-text;"
+    subname.innerText = "r/"+post.permalink.split("/r/")[1].split("/")[0]
+    subname.href = "/./"+subname.innerText
+    container.appendChild(subname)  
+
+    const username = document.createElement("a")
+    username.style = "margin-top: -10px; margin-right: 50px; display: ruby-text;"
+    username.innerText = "u/"+post.author
+    username.href = username.innerText
+    container.appendChild(username)
+
+    const commentCount = document.createElement("a")
+    commentCount.style = "margin-top: -10px; display: ruby-text;"
+    commentCount.innerText = post.numComments +" comments"
+    commentCount.href = post.permalink
+    container.appendChild(commentCount)
+}
+
+function stringToHash(string)
+{
+    let hash = 0
+
+    for (let i=0; i < string.length; i++)
+    {
+        const char = string.charCodeAt(i)
+        hash = (hash << 5) - hash + char
+        hash &= hash
+    }
+
+    return new Uint32Array([hash])[0].toString(36)
 }
